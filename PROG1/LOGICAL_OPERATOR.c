bddf50de44265551cdd867e146b5812c2ec88768
MITM <120081647+SSL-ACTX@users.noreply.github.com>
2023-01-09 14:18:29 +0800

    Update LOGICAL_OPERATOR.c
    Signed-off-by: MITM <120081647+SSL-ACTX@users.noreply.github.com>

--- PROG1/LOGICAL_OPERATOR.c
+++ PROG1/LOGICAL_OPERATOR.c
@@ -2,29 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-	
-	 int x = 10;
-	 int y = 10;
-	 int a = 20;
-	 int b = 21;
-	 
-	if(x==y && a!=b)
-	 {
-	 	printf("x is less than y AND a is equal to b\n");
-	 }
-	 	
-	if(x==y || a==b)
-	 {
-	 	printf("x is less than y OR a is equal to b\n");
-	 }
-	 	
-	if(!(x==y && a==b))
-   {
-	 	printf("NOT OPERATION\n");
-	 }
-	
-	 printf("Codes after IF!");
+int main()
+{
 
-   return 0;
-}
+    int x = 10;
+    int y = 10;
+    int a = 20;
+    int b = 21;
+
+    if(x==y && a!=b)
+    {
+        printf("x is less than y AND a is equal to b\n");
+    }
+    if(x==y || a==b)
+    {
+        printf("x is less than y OR a is equal to b\n");
+    }
+    if(!(x==y && a==b))
+    {
+        printf("NOT OPERATION\n");
+    }
+    if(!(x==y && a==b))
+    {
+        p("Make the EXPRESSION become TRUE \n");
+    }
+
+    printf("\nCodes after IF!");
+
+    return 0;
