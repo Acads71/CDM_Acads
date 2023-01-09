8542d66cf822246653b64ebcabc4deaf954a10e5
MITM <120081647+SSL-ACTX@users.noreply.github.com>
2023-01-09 14:03:20 +0800

    Update LAB_EXAM.c
    Signed-off-by: MITM <120081647+SSL-ACTX@users.noreply.github.com>

--- PROG1/LAB_EXAM.c
+++ PROG1/LAB_EXAM.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define p printf
+#define sc scanf
+
+
 int main()
 {
-   int fir, sec, thr, sum, product;
-   float dif;
+    float num1, num2, num3;
+    float sum, dif, prod;
+    float quo, rs;
 
-   printf("Enter Three Numbers : \n");
-   scanf("%d %d %d",&fir, &sec, &thr);
+    p("Number [1] : ");
+    sc("%f", &n1);
+    p("Number [2] : ");
+    sc("%f", &n2);
+    p("Number [3] : ");
+    sc("%f", &n3);
 
-   sum = fir + sec + thr;
-   dif= (float)(thr + sec)/2;
-   product= fir * thr;
+   //formulas
+    sum = n1+n2+n3;
+    dif = n3-n2;
+    prod = n1*n3;
+    quo = n1/n2;
+    rs = pow(n3,n2);
+    system("cls");
 
-   printf("Sum of 1st, 2nd, and 3rd Numbers is : %d", sum);
-   printf("The difference of %d and %d is: %.2f", thr, sec, dif);
-   printf("Product of 1st and 3rd Number: %d", fir, thr, product);
-
+    p("Sum        = %.2f \n", sum);
+    p("Difference = %.2f \n", dif);
+    p("Product    = %.2f \n", prod);
+    p("Quotient   = %.2f \n", quo);
+    p("Raise      = %.2f \n", rs);
 
     return 0;
 }
