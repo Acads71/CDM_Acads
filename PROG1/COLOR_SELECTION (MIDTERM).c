d101e41b59c1dd7b017ef26bf8912b1cdc28ddb1
???? <120081647+SSL-ACTX@users.noreply.github.com>
2023-01-10 12:13:39 +0800

    Update COLOR_SELECTION (MIDTERM).c

--- PROG1/COLOR_SELECTION (MIDTERM).c
+++ PROG1/COLOR_SELECTION (MIDTERM).c
@@ -3,37 +3,53 @@
 #define p printf
 #define s scanf
 
-   char color;
+char color, dw;
 
-int main() {
+int main()
+{
 
-    p("First letter of a color: ");
-    s("%c", color);
+    do
+    {
+        system("cls");
+        p("\n First letter of a color: ");
+        s("%s", &color);
 
-    if(color=='R'||color=='r') {
-        p("It's color Red!");
-    }
-    else if(color=='O'||color=='o') {
-        p("It's color Orange!");
-    }
-    else if(color=='Y'||color=='y') {
-        p("It's color Yellow!");
-    }
-    else if(color=='G'||color=='g') {
-        p("It's color Green!");
-    }
-    else if(color=='B'||color=='b') {
-        p("It's color Blue!");
-    }
-    else if(color=='I'||color=='i') {
-        p("It's color Indigo!");
-    }
-    else if(color=='V'||color=='v') {
-        p("It's color Violet!");
-    }
-    else {
-        p("Unknow Color!");
-    }
+        if(color=='R'||color=='r')
+        {
+            p("\n It's color Red!");
+        }
+        else if(color=='O'||color=='o')
+        {
+            p("\n It's color Orange!");
+        }
+        else if(color=='Y'||color=='y')
+        {
+            p("\n It's color Yellow!");
+        }
+        else if(color=='G'||color=='g')
+        {
+            p("\n It's color Green!");
+        }
+        else if(color=='B'||color=='b')
+        {
+            p("\n It's color Blue!");
+        }
+        else if(color=='I'||color=='i')
+        {
+            p("\n It's color Indigo!");
+        }
+        else if(color=='V'||color=='v')
+        {
+            p("\n It's color Violet!");
+        }
+        else
+        {
+            p("\n Unknown Color!");
+        }
+
+        p("\n\n Press Y to try again!: [Y/N]\n");
+        s("%s", &dw);
+    } while(dw=='y'||dw=='Y');
 
     return 0;
 }
