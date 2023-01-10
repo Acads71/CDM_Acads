55c9a0985e66ad8f711dca2781c85e46feb5503d
???? <120081647+SSL-ACTX@users.noreply.github.com>
2023-01-10 12:16:36 +0800

    Update AREA OF TRIANGLE.c

--- PROG1/AREA OF TRIANGLE.c
+++ PROG1/AREA OF TRIANGLE.c
@@ -12,16 +12,14 @@ int main()
     {
         system("cls");
         p("\n Area of Triangle \n\n");
-        // Base
         p(" Base = ");
         s("%f", &b);
-        // Height
         p(" Height = ");
         s("%f", &h);
-
+		
         system("cls");
         area = (b*h)/2;
-        // Answer
+
         p("\n\nTotal Area = %.2f", area);
 
         p("\n\n Press Y to try again!: [Y/N]\n");
